2fb192005de168eda3f28ea05b537b02e63ec61e
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2023-05-17 10:46:12 +0900

    navbar MVP prototype

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -3,21 +3,20 @@ import React from 'react'
 
 
 type Options = {
-    options: Array<string>
+  options: Array<string>;
+};
 
-}
-export const DropdownMenu = ({options} : Options) => {
+export const DropdownMenu = ({ options }: Options) => {
   return (
-    <div className='bg-slate-500 w-screen list-none'>   
-        {options.map(x => {
-
-            return x.includes('img') || x.includes('png') ? <li className='w-16 h-16' style={{backgroundImage: `url(/img/${x})`}}></li> : <li>{x}</li> 
-
-        })}
+    <div className="bg-slate-500 w-full left-0 list-none absolute">
+      {options.map((x) => {
+        return x.includes('img') || x.includes('png') ? (
+          <li className="w-16 h-16" style={{ backgroundImage: `url(/img/${x})` }}></li>
+        ) : (
+          <li>{x}</li>
+        );
+      })}
     </div>
-       
-    
-        
-  )
-}
+  );
+};
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,63 +1,117 @@
-import React, { useState }from 'react'
-import { DropdownMenu } from './DropdownMenu'
+import React, { useState } from 'react';
+import { DropdownMenu } from './DropdownMenu';
 
 export const Navbar = () => {
-
-
-  const [isLivingOpen, setIsLivingOpen] =  useState(false)
-  const [isDiningOpen, setIsDiningOpen] =  useState(false)
-  const [isBedroomOpen, setIsBedroomOpen] =  useState(false)
-  const [isServicesOpen, setIsServicesOpen] =  useState(false)
+  const [isLivingOpen, setIsLivingOpen] = useState(false);
+  const [isDiningOpen, setIsDiningOpen] = useState(false);
+  const [isBedroomOpen, setIsBedroomOpen] = useState(false);
+  const [isServicesOpen, setIsServicesOpen] = useState(false);
 
   const handleLiving = () => {
     setIsLivingOpen(!isLivingOpen);
   };
+
+  /*const handleLivingLeave = () => {
+    setIsLivingOpen(false);
+  };*/
+
   const handleDining = () => {
     setIsDiningOpen(!isDiningOpen);
   };
+
+
   const handleBedroom = () => {
     setIsBedroomOpen(!isBedroomOpen);
   };
+
+
   const handleServices = () => {
     setIsServicesOpen(!isServicesOpen);
   };
 
+
+
   return (
     <>
-    <div className="flex flex-1 w-full justify-center">
-      <div className="living relative" onMouseEnter={handleLiving} onMouseLeave={handleLiving}>
-        <button className="bg-gray-200 hover:bg-gray-300 py-2 px-4 rounded">
-          LIVING
-        </button>
-      </div>
-      <div className="dining" onMouseEnter={handleDining} onMouseLeave={handleDining}>
-        <button className="bg-gray-200 hover:bg-gray-300 py-2 px-4 rounded">
-          DINING
-        </button>
-        
-      </div>
-      <div className="bedroom" onMouseEnter={handleBedroom} onMouseLeave={handleBedroom}>
-        <button className="bg-gray-200 hover:bg-gray-300 py-2 px-4 rounded">
-          BEDROOM
-        </button>
-       
-      </div>
-      <div className="services" onMouseEnter={handleServices} onMouseLeave={handleServices}>
-        <button className="bg-gray-200 hover:bg-gray-300 py-2 px-4 rounded">
-          OUR SERVICES
-        </button>
-        
-      </div>
-    </div>
-    <div className="dropdown">
-    {isLivingOpen && <DropdownMenu options={['Shop All','Sofas','Tables','New Arrivals', 'livingroom1.png', 'livingroom2.png']}/>} 
-    {isDiningOpen && <DropdownMenu options={['teeraefe','q34rqewf23e323r','awef2222fe','awqfefwef','aefeavf']}/>}
-    {isBedroomOpen && <DropdownMenu options={['123123123','q3434r','a5466e','aw97f','a2432434f']}/>}
-    {isServicesOpen && <DropdownMenu options={['srgwg4t','q323433455r','asgslkjglkr','werf','a']}/>}
-    </div>
-   
-  </>
+      <div className="flex flex-1 w-full justify-center">
+        <div
+          className="living"
+          onMouseEnter={handleLiving}
+          onMouseLeave={handleLiving}
+        >
+          <button className="bg-gray-200 hover:bg-gray-300 py-2 px-4 rounded"> LIVING </button>
+          {isLivingOpen && (
+            <div
+              className="livingDropdown"
+              onMouseEnter={handleLiving}
+              onMouseLeave={handleLiving}
+            >
+              <DropdownMenu
+                options={['Shop All', 'Sofas', 'Tables', 'New Arrivals', 'livingroom1.png', 'livingroom2.png']}
+              />
+            </div>
+          )}
+        </div>
 
-  )
-  
-}
+        <div
+          className="dining"
+          onMouseEnter={handleDining}
+          onMouseLeave={handleDining}
+        >
+          <button className="bg-gray-200 hover:bg-gray-300 py-2 px-4 rounded"> DINING </button>
+          {isDiningOpen && (
+            <div
+              className="diningDropdown"
+              onMouseEnter={handleDining}
+              onMouseLeave={handleDining}
+            >
+              <DropdownMenu
+                options={['teeraefe', 'q34rqewf23e323r', 'awef2222fe', 'awqfefwef', 'aefeavf']}
+              />
+            </div>
+          )}
+        </div>
+
+        <div
+          className="bedroom"
+          onMouseEnter={handleBedroom}
+          onMouseLeave={handleBedroom}
+        >
+          <button className="bg-gray-200 hover:bg-gray-300 py-2 px-4 rounded"> BEDROOM </button>
+          {isBedroomOpen && (
+            <div
+              className="bedroomDropdown"
+              onMouseEnter={handleBedroom}
+              onMouseLeave={handleBedroom}
+            >
+              <DropdownMenu
+                options={['123123123', 'q3434r', 'a5466e', 'aw97f', 'a2432434f']}
+              />
+            </div>
+          )}
+        </div>
+
+        <div
+          className="services"
+          onMouseEnter={handleServices}
+          onMouseLeave={handleServices}
+        >
+          <button className="bg-gray-200 hover:bg-gray-300 py-2 px-4 rounded">
+OUR SERVICES
+</button>
+{isServicesOpen && (
+<div
+           className="servicesDropdown"
+           onMouseEnter={handleServices}
+           onMouseLeave={handleServices}
+         >
+        <DropdownMenu
+        options={['srgwg4t', 'q323433455r', 'asgslkjglkr', 'werf', 'a']}
+        />
+        </div>
+        )}
+        </div>
+        </div>
+        </>
+        );
+};
